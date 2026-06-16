c95efbbaeb87d5384ad825de774f0502f1e6219e
Hakan Gulec <hakangulec.boun@gmail.com>
2026-06-16 11:05:53 -0400

    Batch SDAP DRQL scheduler forwarding

--- openair2/SDAP/nr_sdap/nr_sdap_sched.c
+++ openair2/SDAP/nr_sdap/nr_sdap_sched.c
@@ -32,6 +32,8 @@
 #define SDAP_DRQL_QUEUE_CLASSES 2
 #define SDAP_DRQL_BLOCK_COOLDOWN_NS (1000 * 1000)
 #define SDAP_DRQL_LOG_BYTE_BUCKET (10 * 1024 * 1024)
+#define SDAP_DRQL_MAX_BATCH_PDUS 32
+#define SDAP_DRQL_MAX_BATCH_BYTES (64 * 1024)
 
 typedef struct sdap_sched_entity_state_s {
   nr_sdap_entity_t *entity;
@@ -56,6 +58,10 @@ static uint64_t sdap_sched_nofit_positive_available;
 static uint64_t sdap_sched_unused_available_bytes;
 static uint64_t sdap_sched_max_observed_queue_bytes;
 static uint64_t sdap_sched_last_util_log_ns;
+static uint64_t sdap_sched_batch_calls;
+static uint64_t sdap_sched_batch_multi_pdu;
+static uint64_t sdap_sched_batch_pdu_limited;
+static uint64_t sdap_sched_batch_byte_limited;
 static nr_sdap_rlc_drql_status_query_t sdap_sched_rlc_status_query;
 static sdap_sched_entity_state_t *sdap_sched_entity_states;
 
@@ -121,7 +127,7 @@ static void nr_sdap_sched_log_util_if_due(bool forwarded)
     sdap_sched_max_observed_queue_bytes = snapshot.queued_bytes;
 
   LOG_I(SDAP,
-        "[DRQL][SDAP Util] passes %llu forwarded_last_pass %d queued_bytes %llu active_queues %u max_queued_bytes %llu forwarded_packets %llu forwarded_bytes %llu queue_peeks %llu available_positive %llu nofit_zero_available %llu nofit_positive_available %llu unused_available_bytes %llu cooldown_skips %llu missing_mapping %llu rlc_query_failures %llu blocked_checks %llu\n",
+        "[DRQL][SDAP Util] passes %llu forwarded_last_pass %d queued_bytes %llu active_queues %u max_queued_bytes %llu forwarded_packets %llu forwarded_bytes %llu queue_peeks %llu available_positive %llu nofit_zero_available %llu nofit_positive_available %llu unused_available_bytes %llu cooldown_skips %llu missing_mapping %llu rlc_query_failures %llu blocked_checks %llu batch_calls %llu batch_multi_pdu %llu batch_pdu_limited %llu batch_byte_limited %llu\n",
         (unsigned long long)sdap_sched_passes,
         forwarded ? 1 : 0,
         (unsigned long long)snapshot.queued_bytes,
@@ -137,7 +143,11 @@ static void nr_sdap_sched_log_util_if_due(bool forwarded)
         (unsigned long long)sdap_sched_cooldown_skips,
         (unsigned long long)sdap_sched_missing_mapping,
         (unsigned long long)sdap_sched_rlc_query_failures,
-        (unsigned long long)sdap_sched_blocked_checks);
+        (unsigned long long)sdap_sched_blocked_checks,
+        (unsigned long long)sdap_sched_batch_calls,
+        (unsigned long long)sdap_sched_batch_multi_pdu,
+        (unsigned long long)sdap_sched_batch_pdu_limited,
+        (unsigned long long)sdap_sched_batch_byte_limited);
 }
 
 void nr_sdap_sched_set_rlc_status_query(nr_sdap_rlc_drql_status_query_t query)
@@ -156,148 +166,175 @@ static void nr_sdap_sched_add_timeout(struct timespec *deadline)
 
 static bool nr_sdap_sched_try_forward_queue(nr_sdap_entity_t *entity, uint8_t queue_class)
 {
-  nr_sdap_dl_queue_head_t head = {0};
-  if (!nr_sdap_dl_peek_sdu(entity, queue_class, &head))
-    return false;
+  bool forwarded_any = false;
+  uint32_t batch_pdus = 0;
+  uint32_t batch_bytes = 0;
 
-  sdap_sched_queue_peeks++;
-  if (head.queue_size > sdap_sched_max_observed_queue_bytes)
-    sdap_sched_max_observed_queue_bytes = head.queue_size;
+  sdap_sched_batch_calls++;
 
-  const uint64_t now_ns = sdap_sched_now_ns();
-  sdap_sched_entity_state_t *state = sdap_sched_get_entity_state(entity);
-  if (state != NULL && state->blocked_until_ns[queue_class] > now_ns) {
-    sdap_sched_cooldown_skips++;
-    return false;
-  }
+  while (batch_pdus < SDAP_DRQL_MAX_BATCH_PDUS && batch_bytes < SDAP_DRQL_MAX_BATCH_BYTES) {
+    nr_sdap_dl_queue_head_t head = {0};
+    if (!nr_sdap_dl_peek_sdu(entity, queue_class, &head))
+      break;
 
-  rb_id_t drb_id = entity->qfi2drb_map(entity, head.qfi);
-  if (drb_id == SDAP_MAP_RULE_EMPTY) {
-    sdap_sched_missing_mapping++;
-    return false;
-  }
+    sdap_sched_queue_peeks++;
+    if (head.queue_size > sdap_sched_max_observed_queue_bytes)
+      sdap_sched_max_observed_queue_bytes = head.queue_size;
 
-  uint32_t limit_bytes = 0;
-  uint32_t occupancy_bytes = 0;
-  uint32_t available_bytes = 0;
-  if (sdap_sched_rlc_status_query == NULL) {
-    sdap_sched_rlc_query_failures++;
-    if (sdap_sched_rlc_query_failures % 1000 == 1)
-      LOG_I(SDAP,
-            "[DRQL][SDAP Sched] RLC query callback missing entity UE %lu head UE %lu DRB %ld QFI %u queue_class %u failures %llu\n",
-            (unsigned long)entity->ue_id,
-            (unsigned long)head.ue_id,
-            (long)drb_id,
-            (unsigned)head.qfi,
-            (unsigned)queue_class,
-            (unsigned long long)sdap_sched_rlc_query_failures);
-    return false;
-  }
-
-  if (!sdap_sched_rlc_status_query((int)head.ue_id, (int)drb_id, &limit_bytes, &occupancy_bytes, &available_bytes)) {
-    sdap_sched_rlc_query_failures++;
-    if (sdap_sched_rlc_query_failures % 1000 == 1)
-      LOG_I(SDAP,
-            "[DRQL][SDAP Sched] RLC query returned false entity UE %lu head UE %lu DRB %ld QFI %u queue_class %u failures %llu\n",
-            (unsigned long)entity->ue_id,
-            (unsigned long)head.ue_id,
-            (long)drb_id,
-            (unsigned)head.qfi,
-            (unsigned)queue_class,
-            (unsigned long long)sdap_sched_rlc_query_failures);
-    return false;
-  }
-
-  if (available_bytes > 0)
-    sdap_sched_available_positive_checks++;
-
-  const uint32_t pdu_bytes = head.sdu_buffer_size + SDAP_HDR_LENGTH;
-  if (pdu_bytes > available_bytes) {
-    if (available_bytes == 0)
-      sdap_sched_nofit_zero_available++;
-    else {
-      sdap_sched_nofit_positive_available++;
-      sdap_sched_unused_available_bytes += available_bytes;
+    const uint64_t now_ns = sdap_sched_now_ns();
+    sdap_sched_entity_state_t *state = sdap_sched_get_entity_state(entity);
+    if (state != NULL && state->blocked_until_ns[queue_class] > now_ns) {
+      sdap_sched_cooldown_skips++;
+      break;
     }
 
-    if (state != NULL)
-      state->blocked_until_ns[queue_class] = now_ns + SDAP_DRQL_BLOCK_COOLDOWN_NS;
+    rb_id_t drb_id = entity->qfi2drb_map(entity, head.qfi);
+    if (drb_id == SDAP_MAP_RULE_EMPTY) {
+      sdap_sched_missing_mapping++;
+      break;
+    }
 
-    sdap_sched_blocked_checks++;
-    if (sdap_sched_blocked_checks <= 10 || sdap_sched_blocked_checks % 100000 == 0)
-      LOG_I(SDAP,
-            "[DRQL][SDAP Sched] blocked UE %lu DRB %ld QFI %u queue_class %u pdu_bytes %u limit %u occupancy %u available %u queue_length %u queue_bytes %u checks %llu\n",
-            (unsigned long)entity->ue_id,
-            (long)drb_id,
-            (unsigned)head.qfi,
-            (unsigned)queue_class,
-            pdu_bytes,
-            limit_bytes,
-            occupancy_bytes,
-            available_bytes,
-            head.queue_length,
-            head.queue_size,
-            (unsigned long long)sdap_sched_blocked_checks);
-    return false;
-  }
+    uint32_t limit_bytes = 0;
+    uint32_t occupancy_bytes = 0;
+    uint32_t available_bytes = 0;
+    if (sdap_sched_rlc_status_query == NULL) {
+      sdap_sched_rlc_query_failures++;
+      if (sdap_sched_rlc_query_failures % 1000 == 1)
+        LOG_I(SDAP,
+              "[DRQL][SDAP Sched] RLC query callback missing entity UE %lu head UE %lu DRB %ld QFI %u queue_class %u failures %llu\n",
+              (unsigned long)entity->ue_id,
+              (unsigned long)head.ue_id,
+              (long)drb_id,
+              (unsigned)head.qfi,
+              (unsigned)queue_class,
+              (unsigned long long)sdap_sched_rlc_query_failures);
+      break;
+    }
 
-  sdap_sdu_pdu_t *item = entity->dl_dequeue_pdu(entity, queue_class);
-  if (item == NULL)
-    return false;
+    if (!sdap_sched_rlc_status_query((int)head.ue_id, (int)drb_id, &limit_bytes, &occupancy_bytes, &available_bytes)) {
+      sdap_sched_rlc_query_failures++;
+      if (sdap_sched_rlc_query_failures % 1000 == 1)
+        LOG_I(SDAP,
+              "[DRQL][SDAP Sched] RLC query returned false entity UE %lu head UE %lu DRB %ld QFI %u queue_class %u failures %llu\n",
+              (unsigned long)entity->ue_id,
+              (unsigned long)head.ue_id,
+              (long)drb_id,
+              (unsigned)head.qfi,
+              (unsigned)queue_class,
+              (unsigned long long)sdap_sched_rlc_query_failures);
+      break;
+    }
 
-  const uint32_t *source_l2_id = item->has_source_l2_id ? &item->source_l2_id : NULL;
-  const uint32_t *destination_l2_id = item->has_destination_l2_id ? &item->destination_l2_id : NULL;
+    if (available_bytes > 0)
+      sdap_sched_available_positive_checks++;
 
-  const bool ret = entity->tx_entity(entity,
-                                     &item->ctxt,
-                                     item->srb_flag,
-                                     item->rb_id,
-                                     item->mui,
-                                     item->confirm,
-                                     item->sdu_buffer_size,
-                                     item->sdu_buffer,
-                                     item->pt_mode,
-                                     source_l2_id,
-                                     destination_l2_id,
-                                     item->qfi,
-                                     item->rqi);
-  if (!ret)
-    LOG_E(SDAP,
-          "[DRQL][SDAP Sched] tx_entity refused UE %lu DRB %ld QFI %u queue_class %u sdu_bytes %u\n",
-          (unsigned long)entity->ue_id,
-          (long)drb_id,
-          (unsigned)item->qfi,
-          (unsigned)queue_class,
-          (unsigned)item->sdu_buffer_size);
+    const uint32_t pdu_bytes = head.sdu_buffer_size + SDAP_HDR_LENGTH;
+    if (pdu_bytes > available_bytes) {
+      if (available_bytes == 0)
+        sdap_sched_nofit_zero_available++;
+      else {
+        sdap_sched_nofit_positive_available++;
+        sdap_sched_unused_available_bytes += available_bytes;
+      }
 
-  if (ret) {
-    if (state != NULL)
-      state->blocked_until_ns[queue_class] = 0;
+      if (state != NULL)
+        state->blocked_until_ns[queue_class] = now_ns + SDAP_DRQL_BLOCK_COOLDOWN_NS;
 
-    const uint64_t forwarded_bytes_before = sdap_sched_forwarded_bytes;
-    sdap_sched_forwarded_packets++;
-    sdap_sched_forwarded_bytes += pdu_bytes;
-    if (sdap_sched_forwarded_packets <= 10
-        || sdap_sched_forwarded_packets % 10000 == 0
-        || sdap_sched_crossed_log_bucket(forwarded_bytes_before, sdap_sched_forwarded_bytes))
-      LOG_I(SDAP,
-            "[DRQL][SDAP Sched] forwarded UE %lu DRB %ld QFI %u queue_class %u pdu_bytes %u limit %u occupancy %u available %u queue_length_before %u queue_bytes_before %u forwarded_packets %llu forwarded_bytes %llu\n",
+      sdap_sched_blocked_checks++;
+      if (sdap_sched_blocked_checks <= 10 || sdap_sched_blocked_checks % 100000 == 0)
+        LOG_I(SDAP,
+              "[DRQL][SDAP Sched] blocked UE %lu DRB %ld QFI %u queue_class %u pdu_bytes %u limit %u occupancy %u available %u queue_length %u queue_bytes %u checks %llu\n",
+              (unsigned long)entity->ue_id,
+              (long)drb_id,
+              (unsigned)head.qfi,
+              (unsigned)queue_class,
+              pdu_bytes,
+              limit_bytes,
+              occupancy_bytes,
+              available_bytes,
+              head.queue_length,
+              head.queue_size,
+              (unsigned long long)sdap_sched_blocked_checks);
+      break;
+    }
+
+    if (batch_bytes + pdu_bytes > SDAP_DRQL_MAX_BATCH_BYTES) {
+      sdap_sched_batch_byte_limited++;
+      break;
+    }
+
+    sdap_sdu_pdu_t *item = entity->dl_dequeue_pdu(entity, queue_class);
+    if (item == NULL)
+      break;
+
+    const uint32_t *source_l2_id = item->has_source_l2_id ? &item->source_l2_id : NULL;
+    const uint32_t *destination_l2_id = item->has_destination_l2_id ? &item->destination_l2_id : NULL;
+
+    const bool ret = entity->tx_entity(entity,
+                                       &item->ctxt,
+                                       item->srb_flag,
+                                       item->rb_id,
+                                       item->mui,
+                                       item->confirm,
+                                       item->sdu_buffer_size,
+                                       item->sdu_buffer,
+                                       item->pt_mode,
+                                       source_l2_id,
+                                       destination_l2_id,
+                                       item->qfi,
+                                       item->rqi);
+    if (!ret)
+      LOG_E(SDAP,
+            "[DRQL][SDAP Sched] tx_entity refused UE %lu DRB %ld QFI %u queue_class %u sdu_bytes %u\n",
             (unsigned long)entity->ue_id,
             (long)drb_id,
             (unsigned)item->qfi,
             (unsigned)queue_class,
-            pdu_bytes,
-            limit_bytes,
-            occupancy_bytes,
-            available_bytes,
-            head.queue_length,
-            head.queue_size,
-            (unsigned long long)sdap_sched_forwarded_packets,
-            (unsigned long long)sdap_sched_forwarded_bytes);
+            (unsigned)item->sdu_buffer_size);
+
+    if (ret) {
+      if (state != NULL)
+        state->blocked_until_ns[queue_class] = 0;
+
+      const uint64_t forwarded_bytes_before = sdap_sched_forwarded_bytes;
+      sdap_sched_forwarded_packets++;
+      sdap_sched_forwarded_bytes += pdu_bytes;
+      batch_pdus++;
+      batch_bytes += pdu_bytes;
+      forwarded_any = true;
+
+      if (sdap_sched_forwarded_packets <= 10
+          || sdap_sched_forwarded_packets % 10000 == 0
+          || sdap_sched_crossed_log_bucket(forwarded_bytes_before, sdap_sched_forwarded_bytes))
+        LOG_I(SDAP,
+              "[DRQL][SDAP Sched] forwarded UE %lu DRB %ld QFI %u queue_class %u pdu_bytes %u limit %u occupancy %u available %u queue_length_before %u queue_bytes_before %u forwarded_packets %llu forwarded_bytes %llu batch_pdus %u batch_bytes %u\n",
+              (unsigned long)entity->ue_id,
+              (long)drb_id,
+              (unsigned)item->qfi,
+              (unsigned)queue_class,
+              pdu_bytes,
+              limit_bytes,
+              occupancy_bytes,
+              available_bytes,
+              head.queue_length,
+              head.queue_size,
+              (unsigned long long)sdap_sched_forwarded_packets,
+              (unsigned long long)sdap_sched_forwarded_bytes,
+              batch_pdus,
+              batch_bytes);
+    }
+
+    nr_sdap_free_queued_sdu(item);
+    if (!ret)
+      break;
   }
 
-  nr_sdap_free_queued_sdu(item);
-  return ret;
+  if (batch_pdus > 1)
+    sdap_sched_batch_multi_pdu++;
+  if (batch_pdus == SDAP_DRQL_MAX_BATCH_PDUS)
+    sdap_sched_batch_pdu_limited++;
+
+  return forwarded_any;
 }
 
 static void nr_sdap_sched_try_forward_entity(nr_sdap_entity_t *entity, void *data)
